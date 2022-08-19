697e868ca6023c56204f8c88bcde5d7df4fb189d
IBassaoud <ismail.bassaoud@gmail.com>
2022-08-19 20:01:15 +0000

    Initialization

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,11 @@
 
 function ageCount() {
+    let ageUser =document.getElementById("age")
     var now =new Date();                            //getting current date
     var currentY= now.getFullYear();                //extracting year from the date
     var currentM= now.getMonth();                   //extracting month from the date
         
-    var dobget =document.getElementById("age").value; //getting user input
+    var dobget =document.getElementById("birthday").value; //getting user input
     var dob= new Date(dobget);                             //formatting input as date
     var prevY= dob.getFullYear();                          //extracting year from input date
     var prevM= dob.getMonth();                             //extracting month from input date
@@ -13,13 +14,16 @@ function ageCount() {
     var ageM =Math.abs(currentM- prevM);          //converting any negative value to positive
         
     document.getElementById('demo').innerHTML = ageY;
+    ageUser.value = ageY
     // document.getElementById('demo').innerHTML = ageY +' years ' + ageM +' months';
 }
 
 function calculateBMI(){
     let height = parseInt(document.getElementById('height').value);
     let weight = parseInt(document.getElementById('weight').value);
-    let bmi = document.getElementById('bim');
+    let bmi = document.getElementById('bmi_display');
+    let p = document.getElementById('bmi_body');
+    let bmiVal = document.getElementById('bmi');
 
     if (height === "" || isNaN(height)) 
         // result.innerHTML = "Provide a valid Height!";
@@ -33,11 +37,24 @@ function calculateBMI(){
         let userBIM = (weight / Math.pow( (height/100), 2 )).toFixed(1)
 
         // Dividing as per the bmi conditions
-        if (userBIM < 18.6) bmi.value = userBIM;
-
-        else if (userBIM >= 18.6 && userBIM < 24.9) 
+        if (userBIM < 18.6){
         bmi.value = userBIM;
-
-        else bmi.value = userBIM;
+        bmiVal.value = userBIM;
+        p.innerHTML = `Under Weight 😒`;
+        p.style.color = "#ffc44d"
+        } else if (userBIM >= 18.6 && userBIM < 24.9) {
+        bmi.value = userBIM
+        bmiVal.value = userBIM;
+        p.innerHTML = `Normal Weight 😍` 
+        p.style.color = "#0be881"
+        
+        }
+        else {
+            bmi.value = userBIM;
+            bmiVal.value = userBIM;
+            p.innerHTML = `Over Weight 😮`
+            p.style.color = "#ff884d" 
+        }
     }
+    
 }
